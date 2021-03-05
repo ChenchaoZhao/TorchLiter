28b68fc11113e7741739168bb89742a73be9b457
ChenchaoZhao <chenchao.zhao@gmail.com>
2021-03-04 23:37:51 -0500

    add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+# load readme
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(name="torch-liter",
+      version="0.0.0",
+      author="Chenchao Zhao",
+      author_email="chenchao.zhao@gmail.com",
+      description="A light weight training tool for pytorch projects.",
+      py_modules=["liter"],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      install_requires=["torch", "numpy"],
+      license="MIT")
