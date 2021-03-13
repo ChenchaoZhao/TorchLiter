cbafd0c5c0b9e483d3b525a84312759217743981
ChenchaoZhao <chenchao.zhao@gmail.com>
2021-03-13 01:15:10 -0500

    change version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="torchliter",
-    version="0.0.2",
+    version="0.0.3",
     author="Chenchao Zhao",
     author_email="chenchao.zhao@gmail.com",
     description="A lightweight training tool for pytorch projects.",
