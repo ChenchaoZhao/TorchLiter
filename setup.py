4210ad9c327f0572a19513f88b09def038d08e10
ChenchaoZhao <chenchao.zhao@gmail.com>
2021-03-13 00:55:53 -0500

    reformat setup

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import find_packages, setup
 
 # load readme
-with open("README.md", 'r') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
-setup(name="torchliter",
-      version="0.0.2",
-      author="Chenchao Zhao",
-      author_email="chenchao.zhao@gmail.com",
-      description="A lightweight training tool for pytorch projects.",
-      packages=find_packages(exclude=["tests"]),
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      install_requires=["torch", "numpy"],
-      license="MIT")
+setup(
+    name="torchliter",
+    version="0.0.2",
+    author="Chenchao Zhao",
+    author_email="chenchao.zhao@gmail.com",
+    description="A lightweight training tool for pytorch projects.",
+    packages=find_packages(exclude=["tests"]),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["torch", "numpy"],
+    license="MIT",
+)
