b8c25f6a2e861e22d644462d07fb3ed5d6af2785
ChenchaoZhao <chenchao.zhao@gmail.com>
2021-03-13 01:08:05 -0500

    add writer test

--- liter/__init__.py
+++ liter/__init__.py
@@ -2,3 +2,4 @@ from .buffer import *
 from .engine import *
 from .stub import *
 from .utils import *
+from .writer import *
---
+++ tests/test_writer.py
@@ -0,0 +1,26 @@
+import os
+import liter
+
+
+def test_writer():
+
+    try:
+        os.remove("tmp.csv")
+    except FileNotFoundError:
+        pass
+
+    with liter.writer.Writer("tmp.csv", ["a", "b"], delimiter=",") as writer:
+        writer({"a": 0.1, "b": 0.2})
+
+    with open("tmp.csv", "r") as f:
+        string = f.read()
+        assert string == "a,b\n0.1,0.2\n"
+
+    with liter.writer.Writer("tmp.csv", ["a", "b"], delimiter=",") as writer:
+        writer({"a": 0.1, "b": 0.2})
+
+    with open("tmp.csv", "r") as f:
+        string = f.read()
+        assert string == "a,b\n0.1,0.2\n0.1,0.2\n"
+
+    os.remove("tmp.csv")
