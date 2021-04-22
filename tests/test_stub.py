3a51287621595177e284abec8b21bc892f7abf2c
ChenchaoZhao <chenchao.zhao@gmail.com>
2021-04-22 00:05:29 -0400

    add test

--- tests/test_stub.py
+++ tests/test_stub.py
@@ -12,3 +12,10 @@ def test_train_stub():
     assert train.iteration == 0
 
     print(train)
+
+
+def test_stub_duplicate():
+
+    assert liter.stub.StubBase(kw="kw")(0) is None
+
+    assert len(liter.stub.StubBase(kw="kw")(10)) == 10
