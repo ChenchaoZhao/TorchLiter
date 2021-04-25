a4aed9066685e97d5172d2f53bdd4ed7f71140d7
ChenchaoZhao <chenchao.zhao@gmail.com>
2021-04-24 22:13:08 -0400

    add factory

--- liter/engine/factory.py
+++ liter/engine/factory.py
@@ -1,2 +1,14 @@
+import inspect
 from .base import EngineBase
 from .buffer import ScalarSmoother
+
+
+def _find_outputs(generator):
+    source = inspect.getsource(generator)
+    names = []
+    for line in source.splitlines():
+        line = line.strip()
+        if line.startswith("yield"):
+            line = line.replace("yield", "")
+            names.append(line.split(",")[0].strip()[1:-1])
+    return names
---
+++ tests/test_factory.py
@@ -0,0 +1,10 @@
+import liter.engine.factory as factory
+
+
+def test_fine_outputs():
+    def _gen():
+        yield "var1", 1
+        yield "var2", 2
+        yield "var3", 3
+
+    assert factory._find_outputs(_gen) == ["var1", "var2", "var3"]
