8a92bdbc7c52c31c7443d0d56599959098896d5b
ChenchaoZhao <chenchao.zhao@gmail.com>
2021-05-07 01:20:56 -0400

    add exceptions

--- liter/exception.py
+++ liter/exception.py
@@ -1,9 +1,35 @@
 import Exception
 
 
-class FatalError(Exception):
+class BreakIteration(Exception):
+    def __init__(self, shutdown_engine=False):
+        self.shutdown_engine = shutdown_engine
+
+
+class ContinueIteration(Exception):
     pass
 
 
-class NonFatalError(Exception):
+class BadBatchError(ContinueIteration):
+    """Use case: current batch of data is corrupted, skip current batch and fetch a new batch and then continue."""
+
+    pass
+
+
+class StopEngine(BreakIteration):
+    """Use case: when raised at batch iteration level, if `shutdown_engine=True` then reraise BreakIteration to terminate engine."""
+
+    def __init__(self):
+        super().__init__(True)
+
+
+class GradientExplosionError(StopEngine):
+    pass
+
+
+class FoundNanError(StopEngine):
+    pass
+
+
+class EarlyStopping(StopEngine):
     pass
