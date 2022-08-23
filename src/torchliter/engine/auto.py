b7c96f7b13e49f0bbb7f1342f192b5ad2c5e9bf6
Chenchao Zhao <35147961+ChenchaoZhao@users.noreply.github.com>
2022-08-22 23:12:56 -0400

    fix-train-eval-steps

--- src/torchliter/engine/auto.py
+++ src/torchliter/engine/auto.py
@@ -1,25 +1,31 @@
-from typing import Callable, Type
+from typing import Callable, Optional, Type
 
 from ..utils import _convert_str_to_py_object_name as _py_name
-from .events import Engine
+from .events import Engine, EventHandler
 
 __all__ = ["AutoEngine"]
-
-
-class EngineMeta(Type):
-    def __new__(cls, name, bases, attrs):
-        name = _py_name(name)
-        attrs = {_py_name(k): v for k, v in attrs.items()}
-        return super().__new__(cls, name, bases, attrs)
 
 
 class AutoEngine(Engine):
     def __init__(self, **kwargs):
         super().__init__()
+        for k, v in kwargs.items():
+            if isinstance(v, EventHandler):
+                self.attach_event(v)
+            else:
+                setattr(self, k, v)
 
     @classmethod
     def _new_engine_type(
-        cls, engine_type_name: str, **methods_to_attach: Callable
+        cls,
+        engine_type_name: str,
+        train_step: Optional[Callable] = None,
+        eval_step: Optional[Callable] = None,
+        **methods_to_attach: Callable
     ) -> Type:
         cls_name = _py_name(engine_type_name)
+        if train_step:
+            methods_to_attach["train_step"] = train_step
+        if eval_step:
+            methods_to_attach["eval_step"] = eval_step
         return type(cls_name, (cls,), methods_to_attach)
